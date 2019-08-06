7a09e66f3964b9537f8985a8266be458f451734b
Aman <aman54kumar@gmail.com>
2019-08-06 19:00:47 +0200

    updating shop page, collection preview component and collection-item component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
 import React from 'react';
-import { Route, BrowserRouter } from 'react-router-dom';
+import { Route, BrowserRouter, Switch } from 'react-router-dom';
 import './App.css';
 import Homepage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 
-
-const Hats = () => {
-  return (
-    <div>
-      <h1>HATS PAGE</h1>
-    </div>
-  )
-}
 
 
 function App() {
   return (
     <BrowserRouter>
-      <div>
-        <Homepage />
-        <Hats />
-      </div>
+      <Switch>
+        <div>
+          <Route exact path='/' component={Homepage} />
+          <Route path='/shop' component={ShopPage} />
+
+        </div>
+      </Switch>
     </BrowserRouter>
   );
 }
--- src/pages/shop/shop.data.js
+++ src/pages/shop/shop.data.js
@@ -1,59 +1,3 @@
-import React from 'react';
-import MenuItem from '../menu-item/menu-item.component'
-import './directory.styles.scss'
-
-class Directory extends React.Component {
-    constructor() {
-        super();
-        this.state = {
-            sections: [
-                {
-                    title: 'hats',
-                    imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
-                    id: 1,
-                    linkUrl: 'hats'
-                },
-                {
-                    title: 'jackets',
-                    imageUrl: 'https://i.ibb.co/px2tCc3/jackets.png',
-                    id: 2,
-                    linkUrl: 'jackets'
-                },
-                {
-                    title: 'sneakers',
-                    imageUrl: 'https://i.ibb.co/0jqHpnp/sneakers.png',
-                    id: 3,
-                    linkUrl: 'sneakers'
-                },
-                {
-                    title: 'womens',
-                    imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
-                    size: 'large',
-                    id: 4,
-                    linkUrl: 'womens'
-                },
-                {
-                    title: 'mens',
-                    imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
-                    size: 'large',
-                    id: 5,
-                    linkUrl: 'shop/mens'
-                }
-            ],
-
-        }
-    }
-    render() {
-        return (
-            <div className='directory-menu'>
-                {this.state.sections.map(({ id, ...otherSectionProps }) =>
-                    <MenuItem key={id} {...otherSectionProps} />
-                )}
-            </div>
-        )
-    }
-}
-
 const SHOP_DATA = [
     {
         id: 1,
@@ -302,4 +246,5 @@ const SHOP_DATA = [
     }
 ]
 
-export default Directory;
+
+export default SHOP_DATA;
